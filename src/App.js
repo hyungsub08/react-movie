ad24b0ea02d67f7daefa9120f0d822d034679800
HS Shin <hyungsub08@gmail.com>
2019-12-09 14:20:21 +0900

    practicing state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Proptypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // state를 쓰기 위해서 component를 extends함
+
+  state = {
+    count: 1
+  };
+
+  
+  add = () => {
+    this.setState(current => ({count: current.count + 1})) ;
+    // 객체 인자를 갖는 함수형를 실행한다고 생각할 것
+  };
+
+  minus = () => {
+    this.setState(current => ({count: current.count - 1})) ;
+    // 이 메소드가 동작할 때 랜더도 새롭게 되어야 값이 변한 것으로 동작을 한다. 
+    // 이를 간편하게 하기 위해서 setState()를 활용하도록 합니다.
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>I'm a class component: {this.state.count}</h1>
+        <button onClick={this.add}> Add </button>
+        <button onClick={this.minus}> Minus </button>
+      </div>
+    );
+  }
 }
 
 export default App;
