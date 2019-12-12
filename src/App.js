0e34c7df6a251e9d80202ca5faace83474774e8f
Hyungsub <hyungsub08@exospheresecurity.com>
2019-12-13 00:48:06 +0900

    change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+
 class App extends React.Component {
     state = {
         isLoading: true,
@@ -11,7 +13,7 @@ class App extends React.Component {
                 data: { movies }
             }
             // ES6 Syntax
-        } = await axios.get("http://yts-proxy.now.sh/list_movies.json");
+        } = await axios.get("http://yts-proxy.now.sh/list_movies.json?sort_by=rating");
         this.setState({movies: movies, isLoading: false});
         // console.log(movies);
     }
@@ -20,10 +22,14 @@ class App extends React.Component {
         this.getMovies();
     }
     render() {
-        const {
-            isLoading
-        } = this.state;
-        return <div > { isLoading ? "Loading" : "We are ready" } </div>
+        const { isLoading, movies } = this.state;
+        // return <div > { isLoading ? "Loading" : "We are ready" } </div>
+        return <div> 
+                { isLoading ? "Loading" : movies.map(movie => {
+                    console.log(movie);
+                    return <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary}></Movie>
+                }) } 
+                </div>
     }
 }
 export default App;
--- src/movie.js
+++ src/movie.js
@@ -3,15 +3,18 @@ import PropTypes from "prop-types";
 
 // state가 필요없기 때문에 function component로 만들어도 됨
 
-function Movvie() {
+function Movie({id, year, title, summary}) {
     return <h1>
-
+        {title}
     </h1>
 }
 
-Movvie.propTypes = {
+Movie.propTypes = {
     id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    // poster: PropTypes.string.isRequired
 }
+
+export default Movie;
